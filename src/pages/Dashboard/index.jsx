c18db9060b781ab2c9c3537f860530c854ab0cc2
Jaydeep Suthar <technicaljade@gmail.com>
2022-10-18 22:33:30 +0530

    Added Cards in Dashboard

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,21 +1,47 @@
-import { FaAngleUp } from 'react-icons/fa';
+import { FaBoxOpen, FaUserAlt } from 'react-icons/fa';
+import { BiTrendingUp } from 'react-icons/bi';
+
+const cardsArr = [
+	{
+		icon: <FaBoxOpen size={'70%'} />,
+		title: 'Active Orders',
+		text: 100,
+	},
+	{
+		icon: <FaUserAlt size={'70%'} />,
+		title: 'Total Users',
+		text: 500,
+	},
+	{
+		icon: <BiTrendingUp size={'70%'} />,
+		title: 'Total Sales',
+		text: 500,
+	},
+];
 
 const DashboardPage = () => {
 	return (
 		<>
 			<h1>Dashboard Page</h1>
 
-			<div>
-				<div className='dashboard-cards card tw-w-72'>
-					<div className='dashboard-card card-body tw-flex tw-flex-row tw-gap-2 tw-p-8'>
-						<div className='icon tw-w-5' style={{border: `1px solid black`}}>
-							<FaAngleUp />
-						</div>
+			<div className='tw-flex tw-flex-row tw-flex-wrap tw-gap-3'>
+				{cardsArr.map((item) => (
+					<>
+						<div className='dashboard-cards card tw-w-72'>
+							<div className='dashboard-card card-body tw-flex tw-flex-row tw-gap-4 tw-p-4'>
+								<div className='icon tw-w-10 tw-flex tw-justify-center tw-items-center'>
+									{/* <FaBoxOpen size={'100%'} /> */}
+									{item.icon}
+								</div>
 
-						<div className='title'>Income</div>
-						<div className='text'>100</div>
-					</div>
-				</div>
+								<div className='tw-flex tw-flex-col tw-gap-1 tw-grow'>
+									<div className='title'>{item.title}</div>
+									<div className='text'>{item.text}</div>
+								</div>
+							</div>
+						</div>
+					</>
+				))}
 			</div>
 		</>
 	);
--- src/pages/User/index.jsx
+++ src/pages/User/index.jsx
@@ -1,7 +1,203 @@
+import { useState } from 'react';
+import { Image, Card, Button } from 'react-bootstrap';
+import {
+	BsLightningCharge,
+	BsLightningChargeFill,
+	BsPencilSquare,
+	BsTrash,
+} from 'react-icons/bs';
+import swal from 'sweetalert';
+
+import products from '../../../misc/product';
+import Table from '../../components/data-table/DataTable';
+import SearchFilter from '../../components/filters/SearchFilter';
+
+const BASE_URL = `http://139.59.22.201/api/static/product/image`;
+
+const toggle_trending = async (id) => {
+	const url = `http://139.59.22.201/dashboard/product_toogle_trending?product_id=${id}`;
+
+	alert('Product is now Trending');
+
+	// try {
+	// 	const response = await axios.get(url);
+
+	// 	if (response.status == 200) {
+	// 		alert(`Trending`);
+	// 		window.location.reload();
+	// 	} else {
+	// 		console.log({ response });
+	// 	}
+	// } catch (err) {
+	// 	console.log({ err });
+	// }
+};
+
+const delete_product = (id) => {
+	swal({
+		title: 'Are you sure?',
+		text: 'Once deleted, you cannot not revert it',
+		icon: 'error',
+		buttons: true,
+		dangerMode: true,
+	}).then(async (value) => {
+		if (value) {
+			alert(`Product is Deleted`);
+		}
+	});
+};
+
+const columns = [
+	{
+		name: 'IMAGE',
+		selector: (row) => row.banner.media,
+		cell: (row) => {
+			let imageURL = `${BASE_URL}/${row.banner.media}`;
+
+			return <Image src={imageURL} width='80px' height={'80px'} />;
+		},
+		// sortable: true,
+		// width: "6em",
+	},
+	{
+		name: 'TITLE',
+		selector: (row) => row.title,
+		sortable: true,
+		// wrap: true,
+		// width: "6em",
+	},
+	{
+		name: 'CATEGORY',
+		selector: (row) => row.category,
+		// sortable: true,
+		wrap: true,
+		width: '12em',
+	},
+	{
+		name: 'STOCK',
+		selector: (row) => row.stock,
+		// sortable: true,
+		// width: "6em",
+	},
+	{
+		name: 'CARTOON',
+		selector: (row) => row.cartoon,
+		// sortable: true,
+		// width: "6em",
+	},
+	{
+		name: 'TRENDING',
+		// selector: (row) => `${row.isTrending}`,
+		cell: (row) => {
+			return (
+				<div
+					className='trending'
+					onClick={() => toggle_trending(row.product_id)}
+				>
+					{row.isTrending ? (
+						<BsLightningChargeFill color='orange' size='16px' />
+					) : (
+						<BsLightningCharge size='16px' />
+					)}
+				</div>
+			);
+		},
+		// sortable: true,
+		// width: "6em",
+	},
+	{
+		name: 'PRICE',
+		selector: (row) => row.price,
+		sortable: true,
+		// width: "6em",
+		wrap: true,
+	},
+	{
+		name: 'MRP',
+		selector: (row) => row.mrp,
+		sortable: true,
+		// width: "6em",
+		wrap: true,
+	},
+	{
+		name: 'DISCOUNT',
+		selector: (row) => `${row.discount_percentage}%`,
+		sortable: true,
+		// width: "6em",
+		wrap: true,
+	},
+	{
+		name: 'ACTIONS',
+		// selector: (row) => `${row.discount_percentage}%`,
+		cell: (row) => {
+			return (
+				<>
+					<BsPencilSquare
+						color='blue'
+						size={17}
+						onClick={() => alert(`Edit Product`)}
+					/>
+
+					<BsTrash
+						color='red'
+						className='mx-3'
+						size={17}
+						onClick={() => delete_product(row.product_id)}
+					/>
+				</>
+			);
+		},
+		// sortable: true,
+		width: '12em',
+	},
+];
+
+const FilterComponent = ({ filterText, setFilterText }) => {
+	return (
+		<>
+			<Card className='mb-3 p-3 d-flex flex-row justify-content-end'>
+				<div>
+					<SearchFilter
+						filterText={filterText}
+						onFilter={(e) => setFilterText(e.target.value)}
+						className='mx-3'
+					/>
+
+					<Button
+						className='tw-bg-indigo-700 hover:tw-bg-indigo-600'
+						size='sm'
+					>
+						Add Product
+					</Button>
+				</div>
+			</Card>
+		</>
+	);
+};
+
 const UserPage = () => {
+	const [filterText, setFilterText] = useState('');
+
+	const filteredData = products.response.filter(
+		(item) =>
+			(item.title &&
+				item.title.toLowerCase().includes(filterText.toLowerCase())) ||
+			(item.category &&
+				item.category.toLowerCase().includes(filterText.toLowerCase()))
+	);
+
 	return (
 		<>
 			<h1>User Page</h1>
+
+			<FilterComponent
+				filterText={filterText}
+				setFilterText={setFilterText}
+			/>
+
+			<Card className='p-2'>
+				<Table columns={columns} data={filteredData} />
+			</Card>
 		</>
 	);
 };
